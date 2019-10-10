68b2d0641545ac38fc5986eb84bcb01f9cc66548
Belbli <andey.dyokin@gmail.com>
2019-10-11 02:48:28 +0300

    rebound

--- Button.cpp
+++ Button.cpp
@@ -7,14 +7,14 @@ Button::Button(){
 }
 
 void Button::setButtonPosition(int cx, int cy, int width, int height) {
-	(*this).cx = cx;
-	(*this).cy = cy;
-	(*this).width = width;
-	(*this).height = height;
+	this->cx = cx;
+	this->cy = cy;
+	this->width = width;
+	this->height = height;
 }
 
 bool Button::isClicked(float xMousePos, float yMousePos){
-	if (xMousePos >= (*this).cx && xMousePos <= (*this).cx + (*this).width && yMousePos >= (*this).cy && yMousePos <= (*this).cy + (*this).height) {
+	if (xMousePos >= this->cx && xMousePos <= this->cx + this->width && yMousePos >= this->cy && yMousePos <= this->cy + this->height) {
 		return true;
 	}
 	return false;
@@ -27,10 +27,10 @@ void Button::CreateButton(int btnId) {
 	glColor3f(1.0, 0.0, 0.0);
 
 	glBegin(GL_POLYGON);
-	glVertex2f((*this).cx, (*this).cy);
-	glVertex2f((*this).cx + (*this).width, (*this).cy);
-	glVertex2f((*this).cx + (*this).width, (*this).cy + (*this).height);
-	glVertex2f((*this).cx, (*this).cy + (*this).height);
+	glVertex2f(this->cx, this->cy);
+	glVertex2f(this->cx + this->width, this->cy);
+	glVertex2f(this->cx + this->width, this->cy + this->height);
+	glVertex2f(this->cx, this->cy + this->height);
 	
 	glEnd();
 }
--- Source.cpp
+++ Source.cpp
@@ -1,65 +1,66 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include<GL/glut.h>
 #include"Button.h"
 #include<math.h>
 #include<vector>
+#include<iostream>
+using namespace std;
 
 #define WIDTH 480
 #define HEIGHT 640
 #define PLAYBTN 1
 #define RECORDSBTN 2
 #define EXITBTN 0
+#define MENUPROC 100
+#define GAMEPROC 101
+#define EXIT 102
 
 using namespace std;
 
-Button playBtn, recordsBtn, exitBtn;
+Button playBtn, recordsBtn, exitBtn , nextLvlBtn, menuBtn, replayBtn;
 
-int x = -1, y = -1, btnWidth = 150, btnHeight = 40, btnState,btnStart, btnPressed = -1, plateWidth = 80, xLeftPlatePos, xRightPlatePos;
-int xMousePos = 0, yPlatePos = 3*HEIGHT/4, xBallPos = 350, yBallPos = yPlatePos, yPrevPlatePos = 0;
-int stateY = 1, stateX = 1;
-int blockAmount = 15;
-bool destroyWnd = false, ballMove = true;
-float xAngle = -2.0, yAngle = -2.0;
+int x = -1, y = -1, btnWidth = 150, btnState,btnStart, btnPressed = -1, plateWidth = 80, xLeftPlatePos, xRightPlatePos;
+int xMousePos = 0, yPlatePos = 3 * HEIGHT / 4 + 5, xBallPos = 350, yBallPos = yPlatePos, yPrevPlatePos = 0, ballR = 7;
+int blockAmount = 1, score = 0;
+bool destroyWnd = false, ballMove = false;
+float xAngle = -2.0, yAngle = -1.0;
+int blockHeight = 150, blockWidth = 140, process = 100;
 
 struct block {
 	int xb;
 	int yb;
-	//int draw;
+	int durability;
 } item;
 
 vector<block> blocks;
 
-void MouseMove(int ax, int ay)
-{
+void MouseMove(int ax, int ay){
 	xMousePos = ax;
 }
 
 void display();
 void timer(int);
 void reshape(int, int);
-void initCoordinates();
+void initBlocks();
 
-void init()
-{
-	glClearColor(0.0, 0.0, 1.0, 1.0);
+void init(){
+	glClearColor(0.8, 0.7, 0.7, 1.0);
 	glOrtho(-360, 360, -360, 360, -100, 100);
 }
 
-void MouseMoveClick(int btn, int state, int ax, int ay)
-{
+void MouseMoveClick(int btn, int state, int ax, int ay){
 	x = ax;
 	y = ay;
 	btnStart = btn;
 	btnState = state;
 }
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]){
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE);
 
 	glutInitWindowPosition(50, 50);
 	glutInitWindowSize(WIDTH, HEIGHT);
-
 	glutCreateWindow("OpenGL");
 	glutDisplayFunc(display);
 	glutReshapeFunc(reshape);
@@ -67,126 +68,142 @@ int main(int argc, char *argv[])
 	glutMouseFunc(MouseMoveClick);
 	glutTimerFunc(0, timer, 0);
 	init();
-	initCoordinates();
+	//initBlocks();
 	glutMainLoop();
 }
 
-void timer(int)
-{
+void timer(int){
 	glutPostRedisplay();
 	glutTimerFunc(1000 / 100, timer, 0);
 }
 
-void showMenu() {
-	int lineSpace = btnHeight + 30;
-	playBtn.setButtonPosition(WIDTH / 4 + btnWidth / 2, HEIGHT / 4, btnWidth, btnHeight);
-	playBtn.CreateButton(PLAYBTN);
-
-	recordsBtn.setButtonPosition(WIDTH / 4 + btnWidth / 2, HEIGHT / 4 + lineSpace, btnWidth, btnHeight);
-	recordsBtn.CreateButton(RECORDSBTN);
-
-	exitBtn.setButtonPosition(WIDTH / 4 + btnWidth / 2, HEIGHT / 4 + 2 * lineSpace, btnWidth, btnHeight);
-	exitBtn.CreateButton(EXITBTN);
-}
-
-void menu() {
-	if (btnPressed == -1) {
-		showMenu();
-		if (playBtn.isClicked(x, y)) {
-			glutSetWindowTitle("Mouse move");
-			btnPressed = 1;
-			glClear(GL_COLOR_BUFFER_BIT);
-		}
-		else
-			glutSetWindowTitle("ove");
-
-		if (exitBtn.isClicked(x, y))
-			destroyWnd = true;
+void renderBM(float x, float y, void *font, char *str){
+	char *c;
+	glRasterPos2f(x, y);
+	for (c = str; *c != '\0'; c++) {
+		glutBitmapCharacter(font, *c);
 	}
 }
 
-void reboundDir() {
+void printText(int score){
+	glColor3f(1.0, 1.0, 1.0);
+	char str[15];
+	_itoa(score, str, 10);
+	renderBM(WIDTH/2, yPlatePos + 100, GLUT_BITMAP_TIMES_ROMAN_24, str);
+}
+
+void printText(char *str) {
+	glColor3f(1.0, 1.0, 1.0);
+	renderBM(WIDTH / 2 - 80, HEIGHT / 2, GLUT_BITMAP_TIMES_ROMAN_24, str);
+}
+
+void showMenu(){
+	int btnHeight = 40;
+	int lineSpace = btnHeight + 30;
+	
+	playBtn.setButtonPosition(WIDTH / 2 - btnWidth / 2, HEIGHT / 4, btnWidth, btnHeight);
+	playBtn.CreateButton(PLAYBTN);
+
+	recordsBtn.setButtonPosition(WIDTH / 2 - btnWidth / 2, HEIGHT / 4 + lineSpace, btnWidth, btnHeight);
+	recordsBtn.CreateButton(RECORDSBTN);
+
+	exitBtn.setButtonPosition(WIDTH / 2 - btnWidth / 2, HEIGHT / 4 + 2 * lineSpace, btnWidth, btnHeight);
+	exitBtn.CreateButton(EXITBTN);
+}
+int t = 0;
+void menu(){
+	if (process == MENUPROC){
+		showMenu();
+		if (playBtn.isClicked(x, y) && btnStart == GLUT_LEFT_BUTTON && btnState == GLUT_UP) {
+			process = GAMEPROC;
+			glClear(GL_COLOR_BUFFER_BIT);
+			btnStart = -1;
+			t++;
+			blockAmount = 1;
+			initBlocks();
+			
+		}
+		if (exitBtn.isClicked(x, y))
+			process = EXIT;
+	}
+}
+
+bool levelPassed() {
+	if (blockAmount == 0)
+		return true;
+	return false;
+}
+
+void reboundDir(){
 	if (yPrevPlatePos < xLeftPlatePos && xAngle > 0 || yPrevPlatePos > xLeftPlatePos && xAngle < 0)
 		yAngle *= -1;
 	else if (yPrevPlatePos < xLeftPlatePos && xAngle < 0 || yPrevPlatePos > xLeftPlatePos && xAngle > 0) {
 		yAngle *= -1;
 		xAngle *= -1;
 	}
-	else {
+	else
 		yAngle *= -1;
-	}
 }
 
-void ballMotion()
-{
-	if (btnStart == GLUT_LEFT_BUTTON)
-		ballMove = true;
-	if (!ballMove) {
-		xBallPos = xLeftPlatePos + plateWidth / 2;
-		yBallPos = yPlatePos - 6;
+bool verticalRebound(int blockIndex) {
+	if (yBallPos + ballR == blocks[blockIndex].yb && xBallPos >= blocks[blockIndex].xb && xBallPos <= blocks[blockIndex].xb + blockWidth || yBallPos - ballR == blocks[blockIndex].yb + blockHeight && xBallPos >= blocks[blockIndex].xb && xBallPos <= blocks[blockIndex].xb + blockWidth){
+		yAngle *= -1;
+		return true;
 	}
-	else{
-		if (yBallPos == yPlatePos && xBallPos >= xLeftPlatePos && xBallPos <= xRightPlatePos + 10){
-			float angle = abs(xRightPlatePos - plateWidth / 2 - xBallPos) / 10 + 1;
-			if (xAngle < 0)
-				xAngle = -angle;
-			else
-				xAngle = angle;
-			reboundDir();
-		}
-		if (xBallPos <= 0 || xBallPos >= WIDTH)
-			xAngle *= -1;
-		
-		if (yBallPos <= 0)
-			yAngle *= -1;
-		if (yBallPos > yPlatePos + 15) {
-			ballMove = false;
-			btnStart = -1;
-		}
-		xBallPos += xAngle;
-		yBallPos += yAngle;
-	}
+	return false;
 }
 
-void collision()
-{
-	int deleteBlock = 0, index = -1;
+void collision(){
 	for (int i = 0; i < blockAmount; i++){
-		if (yBallPos + 10 >= blocks[i].yb && yBallPos + 10 <= blocks[i].yb + 20 && xBallPos >= blocks[i].xb && xBallPos <= blocks[i].xb + 20) {
-			deleteBlock = 1;
-			index = i;
-			reboundDir();
+		if (yBallPos + ballR >= blocks[i].yb && yBallPos - ballR <= blocks[i].yb + blockHeight && xBallPos + ballR >= blocks[i].xb && xBallPos - ballR <= blocks[i].xb + blockWidth) {
+			if (!verticalRebound(i))
+				xAngle *= -1;
+			blocks[i].durability--;
+			score += 20;
 		}
-
+		if (blocks[i].durability == -1) {
+			blocks.erase(blocks.begin() + i);
+			blockAmount--;
 		}
-	if (deleteBlock == 1) {
-		blocks.erase(blocks.begin() + index);
-		blockAmount--;
 	}
-
 }
 
-void initCoordinates()
-{
-	for (int i = 0; i < blockAmount; i++)
-	{
-		item.xb = i * 25 + 10;
+void initBlocks(){
+	blocks.reserve(blockAmount);
+	for (int i = 0; i < blockAmount; i++){
+		item.xb = i * blockWidth + i*5;
 		item.yb = 200;
+		item.durability = i % 4;
 		blocks.push_back(item);
 	}
 }
 
-void ViewBlocks()
-{
-	glColor3f(1.0, 0.0, 1.0);
-	glPointSize(20);
-	for (int i = 0; i < blockAmount; i++)
-	{
-		glBegin(GL_POINTS);
+void switchColor(int color) {
+	if (color == 0) 
+		glColor3f(1.0, 0.0, 0.0);
+	else if (color == 1) 
+		glColor3f(0.7, 0.0, 0.0);
+	else if (color == 2) 
+		glColor3f(0.5, 0.0, 0.0);
+	else
+		glColor3f(0.3, 0.0, 0.0);
+}
+
+void ViewBlocks(){
+	for (int i = 0; i < blockAmount; i++){
+		switchColor(blocks[i].durability);
+		glBegin(GL_POLYGON);
 		glVertex2f(blocks[i].xb, blocks[i].yb);
+		glVertex2f(blocks[i].xb, blocks[i].yb + blockHeight);
+		glVertex2f(blocks[i].xb + blockWidth, blocks[i].yb + blockHeight);
+		glVertex2f(blocks[i].xb + blockWidth, blocks[i].yb);
 		glEnd();
 	}
-
+	if (blockAmount == 0) {
+		glClear(GL_COLOR_BUFFER_BIT);
+		process = MENUPROC;
+		ShowCursor(true);
+	}
 }
 
 void platePosition() {
@@ -209,7 +226,9 @@ void drawPlate() {
 	glBegin(GL_POLYGON);
 
 	glColor3f(1.0, 0.0, 0.0);
+	
 	platePosition();
+
 	glVertex2f(xLeftPlatePos, 20.0 + yPlatePos);
 	glVertex2f(xLeftPlatePos, 0.0 + yPlatePos);
 	glVertex2f(xRightPlatePos, 0.0 + yPlatePos);
@@ -218,36 +237,104 @@ void drawPlate() {
 	glEnd();
 }
 
-void drawBall() {
+void ballMotion(){
+	if (btnStart == GLUT_LEFT_BUTTON) {
+		ballMove = true;
+		btnStart = -1;
+	}
+	if (ballMove == false) {
+		xBallPos = xLeftPlatePos + plateWidth / 2;
+		yBallPos = yPlatePos - 5;
+	}
+	else{
+		if (yBallPos == yPlatePos && xBallPos >= xLeftPlatePos && xBallPos <= xRightPlatePos + 10) {
+			float angle = abs(xRightPlatePos - plateWidth / 2 - xBallPos) / 10 + 1;
+			if (xAngle < 0)
+				xAngle = -angle;
+			else
+				xAngle = angle;
+			reboundDir();
+		}
+		if (xBallPos <= 0 || xBallPos >= WIDTH)
+			xAngle *= -1;
+
+		if (yBallPos <= 0)
+			yAngle *= -1;
+		if (yBallPos > yPlatePos + 15) {
+			ballMove = false;
+			btnStart = -1;
+		}
+		xBallPos += xAngle;
+		yBallPos += yAngle;
+	}
+}
+
+void drawBall(){
 	ballMotion();
-	glBegin(GL_POINTS);
-	glColor3f(1.0, 1.0, 0.0);
-	
-	glVertex2f(xBallPos, yBallPos);
+	glColor3f(0.0, 1.0, 0.0);
+	glBegin(GL_TRIANGLE_FAN);
+	for (int i = 0; i <= 50; i++) {
+		float angle = 2.0 * 3.1415926 * float(i) / float(50);
+		float dx = ballR * cosf(angle);
+		float dy = ballR * sinf(angle);
+		glVertex2f(xBallPos + dx, yBallPos + dy);
+	}
 	glEnd();
 }
 
-void display()
-{
+void game() {
+	ShowCursor(false);
+	printText(score);
+	collision();
+	ViewBlocks();
+	drawPlate();
+	drawBall();
+}
+
+void display(){
 	glClear(GL_COLOR_BUFFER_BIT);
 	glLoadIdentity();
-	menu();
-	if (btnPressed == 1) {
+	switch (process) {
+		case MENUPROC:
+			ShowCursor(true);
+			menu();
+			break;
+		case GAMEPROC:
+			glClear(GL_COLOR_BUFFER_BIT);
+			game();
+			break;
+		case EXIT:
+			glutDestroyWindow(1);
+			break;
+	}
+	/*	menu();
+	if (btnPressed == 1){
 		ShowCursor(false);
+		printText(score);
 		collision();
 		ViewBlocks();
 
-		glPointSize(10); 
-		drawBall();
 		drawPlate();
+		drawBall();
 	}
-	glutSwapBuffers();
-	if (destroyWnd)
-		glutDestroyWindow(1);
+	if (levelPassed()) {
+		ShowCursor(true);
+		printText("Level Passed!");
+		menuBtn.setButtonPosition(WIDTH / 2 - 70/2, HEIGHT / 2 + 50, 70, 40);
+		menuBtn.CreateButton(10);
+		if (menuBtn.isClicked(x, y) && btnStart == GLUT_LEFT_BUTTON && btnState == GLUT_UP) {
+			menu();
+			blockAmount = 10;
+		}
+	}
+	*/
+	/*if (destroyWnd)
+		glutDestroyWindow(1);*/
+	if(process != EXIT)
+		glutSwapBuffers();
 }
 
-void reshape(int w, int h)
-{
+void reshape(int w, int h){
 	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
 	gluOrtho2D(0, WIDTH, HEIGHT,0);
