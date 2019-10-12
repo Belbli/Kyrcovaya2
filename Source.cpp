9377a830b78136a1a682090ad83cb31d1d177cb4
Belbli <andey.dyokin@gmail.com>
2019-10-12 23:06:48 +0300

    adding lvls feom file

--- Source.cpp
+++ Source.cpp
@@ -7,13 +7,14 @@
 using namespace std;
 
 #define WIDTH 480
-#define HEIGHT 640
+#define HEIGHT 600
 #define PLAYBTN 1
 #define RECORDSBTN 2
 #define EXITBTN 0
 #define MENUPROC 100
 #define GAMEPROC 101
-#define EXIT 102
+#define SUBMENUPROC 102
+#define EXIT 110
 
 using namespace std;
 
@@ -21,11 +22,11 @@ Button playBtn, recordsBtn, exitBtn , nextLvlBtn, menuBtn, replayBtn;
 
 int x = -1, y = -1, btnWidth = 150, btnState,btnStart, btnPressed = -1, plateWidth = 80, xLeftPlatePos, xRightPlatePos;
 int xMousePos = 0, yPlatePos = 3 * HEIGHT / 4 + 5, xBallPos = 350, yBallPos = yPlatePos, yPrevPlatePos = 0, ballR = 7;
-int blockAmount = 1, score = 0;
+int blockAmount = 0, score = 0;
 bool destroyWnd = false, ballMove = false;
 float xAngle = -2.0, yAngle = -1.0;
-int blockHeight = 150, blockWidth = 140, process = 100;
-
+int blockHeight = 120, blockWidth = 150, process = 100;
+//50 20
 struct block {
 	int xb;
 	int yb;
@@ -68,7 +69,6 @@ int main(int argc, char *argv[]){
 	glutMouseFunc(MouseMoveClick);
 	glutTimerFunc(0, timer, 0);
 	init();
-	//initBlocks();
 	glutMainLoop();
 }
 
@@ -97,33 +97,82 @@ void printText(char *str) {
 	renderBM(WIDTH / 2 - 80, HEIGHT / 2, GLUT_BITMAP_TIMES_ROMAN_24, str);
 }
 
-void showMenu(){
+char *loadLevel(char *path) {
+	FILE *file;
+	file = fopen(path, "r");
+
+	fseek(file, 0, SEEK_END);
+	int size = ftell(file);
+	int buffSize = WIDTH / blockWidth;
+	fseek(file, 0, SEEK_SET);
+	char *buff = (char*)calloc(buffSize, sizeof(char));
+	char *dest = (char*)calloc(size, sizeof(char));
+	while (fgets(buff, buffSize, file) != NULL) {
+		//fgets(buff, buffSize, file);
+		strcat(dest, buff);
+	}
+	return dest;
+}
+
+void initMenuButtons() {
 	int btnHeight = 40;
 	int lineSpace = btnHeight + 30;
-	
+
 	playBtn.setButtonPosition(WIDTH / 2 - btnWidth / 2, HEIGHT / 4, btnWidth, btnHeight);
-	playBtn.CreateButton(PLAYBTN);
-
 	recordsBtn.setButtonPosition(WIDTH / 2 - btnWidth / 2, HEIGHT / 4 + lineSpace, btnWidth, btnHeight);
-	recordsBtn.CreateButton(RECORDSBTN);
-
 	exitBtn.setButtonPosition(WIDTH / 2 - btnWidth / 2, HEIGHT / 4 + 2 * lineSpace, btnWidth, btnHeight);
+}
+
+void showMenu(){
+	initMenuButtons();
+	playBtn.CreateButton(PLAYBTN);
+	recordsBtn.CreateButton(RECORDSBTN);
 	exitBtn.CreateButton(EXITBTN);
 }
-int t = 0;
+
+void initSubMenuButtons() {
+	int subMenuBtnH = 40, subMenuBtnW = 80;
+	menuBtn.setButtonPosition(WIDTH / 2 - subMenuBtnW / 2, HEIGHT / 2, subMenuBtnW, subMenuBtnH);
+	nextLvlBtn.setButtonPosition(WIDTH / 2 + subMenuBtnW, HEIGHT / 2, subMenuBtnW, subMenuBtnH);
+}
+
+void addSubMenuButtons() {
+	nextLvlBtn.CreateButton(16);
+	menuBtn.CreateButton(15);
+}
+
+void subMenu() {
+	char *lvlPath;
+	ShowCursor(true);
+	glClear(GL_COLOR_BUFFER_BIT);
+	initSubMenuButtons();
+	addSubMenuButtons();
+	if (menuBtn.isClicked(x, y) && btnStart == GLUT_LEFT_BUTTON && btnState == GLUT_UP) {
+		process = MENUPROC;
+		btnStart = -1;
+	}
+	else if (nextLvlBtn.isClicked(x, y) && btnStart == GLUT_LEFT_BUTTON && btnState == GLUT_UP) {
+		initBlocks();
+		process = GAMEPROC;
+		btnStart = -1;
+		ballMove = false;
+		score = 0;
+	}
+}
+
 void menu(){
+	char *lvlPath = NULL;
+	ShowCursor(true);
 	if (process == MENUPROC){
 		showMenu();
 		if (playBtn.isClicked(x, y) && btnStart == GLUT_LEFT_BUTTON && btnState == GLUT_UP) {
 			process = GAMEPROC;
 			glClear(GL_COLOR_BUFFER_BIT);
 			btnStart = -1;
-			t++;
-			blockAmount = 1;
 			initBlocks();
-			
+			score = 0;
 		}
-		if (exitBtn.isClicked(x, y))
+		else if (exitBtn.isClicked(x, y) && btnStart == GLUT_LEFT_BUTTON && btnState == GLUT_UP)
 			process = EXIT;
 	}
 }
@@ -161,29 +210,51 @@ void collision(){
 			blocks[i].durability--;
 			score += 20;
 		}
-		if (blocks[i].durability == -1) {
+		if (blocks[i].durability == 0) {
 			blocks.erase(blocks.begin() + i);
 			blockAmount--;
 		}
 	}
 }
 
-void initBlocks(){
-	blocks.reserve(blockAmount);
-	for (int i = 0; i < blockAmount; i++){
-		item.xb = i * blockWidth + i*5;
-		item.yb = 200;
-		item.durability = i % 4;
-		blocks.push_back(item);
+void setBlockAmount(char *lvl) {
+	for (int i = 0; i < strlen(lvl); i++) {
+		if (lvl[i] != '0' && lvl[i] != '\n')
+			blockAmount++;
+	}
+}
+
+void initBlocks() {
+	char *lvlMap = loadLevel("lvl_11.txt");
+	int ySpacing = 0, t = 0, xSpasing = 0;
+	setBlockAmount(lvlMap);
+	if (blockAmount > 0)
+		ySpacing = 0;
+	for (int i = 0; i < strlen(lvlMap); i++){
+		if (lvlMap[i] == '\n') {
+			ySpacing += blockHeight + 5;
+			t = 0;
+			xSpasing = 0;
+		}
+		if(lvlMap[i] != '0' && lvlMap[i] != '\n') {
+			item.xb = xSpasing;
+			item.yb = ySpacing;
+			item.durability = lvlMap[i] - 48;
+			blocks.push_back(item);
+			t++;
+			xSpasing += blockWidth + 5;
+		}
+		if(lvlMap[i] == '0')
+			xSpasing += blockWidth + 5;
 	}
 }
 
 void switchColor(int color) {
-	if (color == 0) 
+	if (color == 1) 
 		glColor3f(1.0, 0.0, 0.0);
-	else if (color == 1) 
-		glColor3f(0.7, 0.0, 0.0);
 	else if (color == 2) 
+		glColor3f(0.7, 0.0, 0.0);
+	else if (color == 3) 
 		glColor3f(0.5, 0.0, 0.0);
 	else
 		glColor3f(0.3, 0.0, 0.0);
@@ -201,8 +272,11 @@ void ViewBlocks(){
 	}
 	if (blockAmount == 0) {
 		glClear(GL_COLOR_BUFFER_BIT);
-		process = MENUPROC;
+		process = SUBMENUPROC;
 		ShowCursor(true);
+		ballMove = false;
+		blocks.clear();
+		blockAmount = 0;
 	}
 }
 
@@ -296,8 +370,10 @@ void display(){
 	glLoadIdentity();
 	switch (process) {
 		case MENUPROC:
-			ShowCursor(true);
 			menu();
+			break;
+		case SUBMENUPROC:
+			subMenu();
 			break;
 		case GAMEPROC:
 			glClear(GL_COLOR_BUFFER_BIT);
@@ -307,29 +383,6 @@ void display(){
 			glutDestroyWindow(1);
 			break;
 	}
-	/*	menu();
-	if (btnPressed == 1){
-		ShowCursor(false);
-		printText(score);
-		collision();
-		ViewBlocks();
-
-		drawPlate();
-		drawBall();
-	}
-	if (levelPassed()) {
-		ShowCursor(true);
-		printText("Level Passed!");
-		menuBtn.setButtonPosition(WIDTH / 2 - 70/2, HEIGHT / 2 + 50, 70, 40);
-		menuBtn.CreateButton(10);
-		if (menuBtn.isClicked(x, y) && btnStart == GLUT_LEFT_BUTTON && btnState == GLUT_UP) {
-			menu();
-			blockAmount = 10;
-		}
-	}
-	*/
-	/*if (destroyWnd)
-		glutDestroyWindow(1);*/
 	if(process != EXIT)
 		glutSwapBuffers();
 }
